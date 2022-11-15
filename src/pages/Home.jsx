f2668bdb0d5b0f2406ec1d527150a15b396ca351
Raihan567 <aburaihansarker567@gmail.com>
2022-11-15 16:46:27 +0600

    service section design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
-import Helmet from '../components/Helmet/Helmet'
-import '../styles/Home.css'
-
+import React from "react";
+import { Col, Container, Row } from "reactstrap";
+import Helmet from "../components/Helmet/Helmet";
+import "../styles/Home.css";
+import heroImg from "../assets/images/hero-img.png";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import Services from "../services/Services";
 const Home = () => {
+  const year = new Date().getFullYear();
   return (
-    <Helmet title="Home">Home</Helmet>
-  )
-}
+    <Helmet title="Home">
+      <section className="hero__section">
+        <Container>
+          <Row>
+            {/* Hero Section */}
+            <Col lg="6">
+              <div className="hero__content">
+                <p className="hero__subtitle">Trending Product in {year}</p>
+                <h2>Make your Interior More minimalistic & modern</h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. A
+                  enim nam nisi blanditiis, ipsam quae optio dolore natus
+                  quisquam eligendi.
+                </p>
+                <motion.button whileTap={{ scale: 1.1 }} className="buy__btn">
+                  <Link to="/shop">SHOP NOW</Link>
+                </motion.button>
+              </div>
+            </Col>
+            <Col lg="6">
+              <div className="hero__img">
+                <img src={heroImg} alt="" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
 
-export default Home
+      {/* Service Section */}
+      <Services />
+    </Helmet>
+  );
+};
+
+export default Home;
---
+++ src/services/Services.jsx
@@ -0,0 +1,35 @@
+import { motion } from "framer-motion";
+import React from "react";
+import { Col, Container, Row } from "reactstrap";
+import serviceDetails from "../assets/data/serviceData";
+import "./Services.css";
+
+const Services = () => {
+  return (
+    <section className="service__section">
+      <Container>
+        <Row>
+          {serviceDetails.map((item, index) => (
+            <Col lg="3" md="4" key={index}>
+              <motion.div
+                whileHover={{ scale: 1.2 }}
+                className="service__item"
+                style={{ background: `${item.bg}` }}
+              >
+                <span>
+                  <i className={item.icon}></i>
+                </span>
+                <div>
+                  <h3>{item.title}</h3>
+                  <p>{item.subtitle}</p>
+                </div>
+              </motion.div>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default Services;
